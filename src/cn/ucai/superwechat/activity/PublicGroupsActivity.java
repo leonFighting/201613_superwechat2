f89e6b5b960d525f42701790b69c568b5a9f0036
leonFighting <379029170@qq.com>
2016-05-29 15:42:19 +0800

    1.初始化公开群组initView、initData、setListener方法2.修改adapter适配公开群组activity 3.添加广播接收器，确认分页下载

--- src/cn/ucai/superwechat/activity/PublicGroupsActivity.java
+++ src/cn/ucai/superwechat/activity/PublicGroupsActivity.java
@@ -1,10 +1,10 @@
 /**
  * Copyright (C) 2013-2014 EaseMob Technologies. All rights reserved.
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,11 +15,13 @@
 package cn.ucai.superwechat.activity;
 
 import java.util.ArrayList;
-import java.util.List;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,7 +29,7 @@ import android.widget.AbsListView.OnScrollListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AbsListView;
-import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -35,166 +37,227 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.easemob.chat.EMCursorResult;
-import com.easemob.chat.EMGroupInfo;
-import com.easemob.chat.EMGroupManager;
+
+import com.android.volley.toolbox.NetworkImageView;
+
 import cn.ucai.superwechat.R;
-import com.easemob.exceptions.EaseMobException;
+import cn.ucai.superwechat.SuperWeChatApplication;
+import cn.ucai.superwechat.bean.Group;
+import cn.ucai.superwechat.bean.User;
+import cn.ucai.superwechat.task.DownloadPublicGroupTask;
+import cn.ucai.superwechat.utils.UserUtils;
+
 
 public class PublicGroupsActivity extends BaseActivity {
-	private ProgressBar pb;
-	private ListView listView;
-	private GroupsAdapter adapter;
-	
-	private List<EMGroupInfo> groupsList;
-	private boolean isLoading;
-	private boolean isFirstLoading = true;
-	private boolean hasMoreData = true;
-	private String cursor;
-	private final int pagesize = 20;
+    public static final String TAG = PublicGroupsActivity.class.getName();
+    private ProgressBar pb;
+    private ListView listView;
+    private GroupsAdapter adapter;
+
+    private ArrayList<Group> groupsList;
+    private boolean isLoading;
+    private boolean isFirstLoading = true;
+    private boolean hasMoreData = true;
+    private final int pagesize = 20;
     private LinearLayout footLoadingLayout;
     private ProgressBar footLoadingPB;
     private TextView footLoadingText;
     private Button searchBtn;
-    
+    PublicGroupChangedReceiver mPublicGroupChangedReceiver;
+    PublicGroupsActivity mContext;
+    int pageId = 0;
+    NetworkImageView mivAvatar;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_public_groups);
-
-		pb = (ProgressBar) findViewById(R.id.progressBar);
-		listView = (ListView) findViewById(R.id.list);
-		groupsList = new ArrayList<EMGroupInfo>();
-		searchBtn = (Button) findViewById(R.id.btn_search);
-		
-		View footView = getLayoutInflater().inflate(R.layout.listview_footer_view, null);
-        footLoadingLayout = (LinearLayout) footView.findViewById(R.id.loading_layout);
-        footLoadingPB = (ProgressBar)footView.findViewById(R.id.loading_bar);
-        footLoadingText = (TextView) footView.findViewById(R.id.loading_text);
-        listView.addFooterView(footView, null, false);
-        footLoadingLayout.setVisibility(View.GONE);
-        
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_public_groups);
+        mContext = this;
+        initView();
         //获取及显示数据
-        loadAndShowData();
-        
+//        loadAndShowData();
+        setListener();
+        registerPublicGroupChangedReceiver();
+    }
+
+    private void setListener() {
+        setOnItemClickListener();
+        setOnScrollListener();
+    }
+
+    private void setOnItemClickListener() {
         //设置item点击事件
         listView.setOnItemClickListener(new OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 startActivity(new Intent(PublicGroupsActivity.this, GroupSimpleDetailActivity.class).
-                        putExtra("groupinfo", adapter.getItem(position)));
+                        putExtra("groupinfo", adapter.mGroupList.get(position)));
             }
         });
+    }
+
+    private void setOnScrollListener() {
         listView.setOnScrollListener(new OnScrollListener() {
-            
+            //上拉刷新
             @Override
             public void onScrollStateChanged(AbsListView view, int scrollState) {
-                if(scrollState == OnScrollListener.SCROLL_STATE_IDLE){
-                    if(listView.getCount() != 0){
+                if (scrollState == OnScrollListener.SCROLL_STATE_IDLE) {
+                    if (listView.getCount() != 0) {
                         int lasPos = view.getLastVisiblePosition();
-                        if(hasMoreData && !isLoading && lasPos == listView.getCount()-1){
+                        if (hasMoreData && !isLoading && lasPos == listView.getCount() - 1) {
+                            pageId += pagesize;
+                            User user = SuperWeChatApplication.getInstance().getUser();
+                            new DownloadPublicGroupTask(mContext, "", pageId, pagesize);
                             loadAndShowData();
                         }
                     }
                 }
             }
-            
+
             @Override
             public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-                
+
             }
         });
-        
-	}
-	
-	/**
-	 * 搜索
-	 * @param view
-	 */
-	public void search(View view){
-	    startActivity(new Intent(this, PublicGroupsSeachActivity.class));
-	}
-	
-	private void loadAndShowData(){
-	    new Thread(new Runnable() {
+    }
 
-            public void run() {
-                try {
-                    isLoading = true;
-                    final EMCursorResult<EMGroupInfo> result = EMGroupManager.getInstance().getPublicGroupsFromServer(pagesize, cursor);
-                    //获取group list
-                    final List<EMGroupInfo> returnGroups = result.getData();
-                    runOnUiThread(new Runnable() {
+    private void initView() {
+        pb = (ProgressBar) findViewById(R.id.progressBar);
+        listView = (ListView) findViewById(R.id.list);
+        groupsList = new ArrayList<Group>();
+        searchBtn = (Button) findViewById(R.id.btn_search);
 
-                        public void run() {
-                            searchBtn.setVisibility(View.VISIBLE);
-                            groupsList.addAll(returnGroups);
-                            if(returnGroups.size() != 0){
-                                //获取cursor
-                                cursor = result.getCursor();
-                                if(returnGroups.size() == pagesize)
-                                    footLoadingLayout.setVisibility(View.VISIBLE);
-                            }
-                            if(isFirstLoading){
-                                pb.setVisibility(View.INVISIBLE);
-                                isFirstLoading = false;
-                                //设置adapter
-                                adapter = new GroupsAdapter(PublicGroupsActivity.this, 1, groupsList);
-                                listView.setAdapter(adapter);
-                            }else{
-                                if(returnGroups.size() < pagesize){
-                                    hasMoreData = false;
-                                    footLoadingLayout.setVisibility(View.VISIBLE);
-                                    footLoadingPB.setVisibility(View.GONE);
-                                    footLoadingText.setText("No more data");
-                                }
-                                adapter.notifyDataSetChanged();
-                            }
-                            isLoading = false;
-                        }
-                    });
-                } catch (EaseMobException e) {
-                    e.printStackTrace();
-                    runOnUiThread(new Runnable() {
-                        public void run() {
-                            isLoading = false;
-                            pb.setVisibility(View.INVISIBLE);
-                            footLoadingLayout.setVisibility(View.GONE);
-                            Toast.makeText(PublicGroupsActivity.this, "加载数据失败，请检查网络或稍后重试", 0).show();
-                        }
-                    });
+        View footView = getLayoutInflater().inflate(R.layout.listview_footer_view, null);
+        footLoadingLayout = (LinearLayout) footView.findViewById(R.id.loading_layout);
+        footLoadingPB = (ProgressBar) footView.findViewById(R.id.loading_bar);
+        footLoadingText = (TextView) footView.findViewById(R.id.loading_text);
+        listView.addFooterView(footView, null, false);
+        footLoadingLayout.setVisibility(View.GONE);
+    }
+
+    /**
+     * 搜索
+     *
+     * @param view
+     */
+    public void search(View view) {
+        startActivity(new Intent(this, PublicGroupsSeachActivity.class));
+    }
+
+    private void loadAndShowData() {
+        try {
+            isLoading = true;
+            ArrayList<Group> publicGroupList = SuperWeChatApplication.getInstance().getPublicGroupList();
+            Log.e(TAG, "loadAndShowData,publicGroupList=" + publicGroupList.size() + ",groupsList=" + groupsList.size());
+            for (Group g : publicGroupList) {
+                if (!groupsList.contains(g)) {
+                    groupsList.add(g);
                 }
             }
-        }).start();
-	}
-	/**
-	 * adapter
-	 *
-	 */
-	private class GroupsAdapter extends ArrayAdapter<EMGroupInfo> {
+            searchBtn.setVisibility(View.VISIBLE);
+            //全局变量不等于0
+            if (groupsList.size() != 0) {
+                if (groupsList.size() == publicGroupList.size())
+                    footLoadingLayout.setVisibility(View.VISIBLE);
+            }
+            //是否第一次加载
+            if (isFirstLoading) {
+                pb.setVisibility(View.INVISIBLE);
+                isFirstLoading = false;
+                //设置adapter
+                adapter = new GroupsAdapter(PublicGroupsActivity.this, 1, groupsList);
+                listView.setAdapter(adapter);
+            } else {
+                if (groupsList.size() < (pageId + 1) * pagesize) {
+                    //是否再次重新添加数据
+                    hasMoreData = false;
+                    footLoadingLayout.setVisibility(View.VISIBLE);
+                    footLoadingPB.setVisibility(View.GONE);
+                    footLoadingText.setText("No more data");
+                }
+                adapter.notifyDataSetChanged();
+            }
+            isLoading = false;
+        } catch (Exception e){
+            e.printStackTrace();
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    isLoading = false;
+                    pb.setVisibility(View.INVISIBLE);
+                    footLoadingLayout.setVisibility(View.GONE);
+                    Toast.makeText(PublicGroupsActivity.this, "加载数据失败，请检查网络或稍后重试", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
 
-		private LayoutInflater inflater;
+    public class GroupsAdapter extends BaseAdapter {
 
-		public GroupsAdapter(Context context, int res, List<EMGroupInfo> groups) {
-			super(context, res, groups);
-			this.inflater = LayoutInflater.from(context);
-		}
+        private LayoutInflater inflater;
+        Context mContext;
+        ArrayList<Group> mGroupList;
 
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			if (convertView == null) {
-				convertView = inflater.inflate(R.layout.row_group, null);
-			}
+        public GroupsAdapter(Context context, int res, ArrayList<Group> groups) {
+            this.mContext = context;
+            this.mGroupList = groups;
+            this.inflater = LayoutInflater.from(context);
+        }
 
-			((TextView) convertView.findViewById(R.id.name)).setText(getItem(position).getGroupName());
+        @Override
+        public int getCount() {
+            return mGroupList == null ? 0 : mGroupList.size();
+        }
 
-			return convertView;
-		}
-	}
-	
-	public void back(View view){
-		finish();
-	}
+        @Override
+        public Group getItem(int i) {
+            return mGroupList.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.row_group, null);
+            }
+
+            ((TextView) convertView.findViewById(R.id.name)).setText(getItem(position).getMGroupName());
+            mivAvatar = (NetworkImageView) convertView.findViewById(R.id.avatar);
+            UserUtils.setGroupBeanAvatar(mGroupList.get(position).getMGroupHxid(), mivAvatar);
+            return convertView;
+        }
+    }
+
+
+    public void back(View view) {
+        finish();
+    }
+
+    class PublicGroupChangedReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //第一次进来，刷新当前页面
+            loadAndShowData();
+        }
+
+    }
+
+    public void registerPublicGroupChangedReceiver() {
+        mPublicGroupChangedReceiver = new PublicGroupChangedReceiver();
+        IntentFilter filter = new IntentFilter("update_public_group");
+        registerReceiver(mPublicGroupChangedReceiver, filter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mPublicGroupChangedReceiver != null) {
+            unregisterReceiver(mPublicGroupChangedReceiver);
+        }
+    }
 }
